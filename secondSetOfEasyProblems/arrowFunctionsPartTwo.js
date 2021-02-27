e2ab0c42eace44e2d36fcf2c473247d8ebc6244e
Rafiq Kamal <71498824+Rafiq@users.noreply.github.com>
2021-02-27 10:38:06 +0300

    Create arrowFunctionsPartTwo.js

---
+++ secondSetOfEasyProblems/arrowFunctionsPartTwo.js
@@ -0,0 +1,12 @@
+let initGame = () => {
+    return {
+      level: 1,
+      score: 0
+    }
+  };
+
+  let game = initGame();
+
+  console.log('Level: ' + game.level);
+  console.log('Score: ' + game.score);
+
