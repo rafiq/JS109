56edabfffd66d19a7013b8d6aa0a8e53c0fe86c2
Rafiq Kamal <71498824+Rafiq@users.noreply.github.com>
2021-03-04 15:35:48 +0300

    Update exercises.js

--- secondSetOfEasyProblems/exercises.js
+++ secondSetOfEasyProblems/exercises.js
@@ -66,10 +66,12 @@ function arrayToList(array) {
     }
     return list;
 }
-
+function listToArray(list) {
+    return Object.keys(list).map((key) => list[key])
+}
 console.log(arrayToList([1,5,10,20,25,30,40]));
 // → {value: 10, rest: {value: 20, rest: null}}
-// console.log(listToArray(arrayToList([10, 20, 30])));
+console.log(listToArray(arrayToList([10, 20, 30])));
 // // → [10, 20, 30]
 // console.log(prepend(10, prepend(20, null)));
 // // → {value: 10, rest: {value: 20, rest: null}}
