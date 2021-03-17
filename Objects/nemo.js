c56da402fbd609b813e2c4c04b5e0366c5d7efce
Rafiq Kamal <71498824+Rafiq@users.noreply.github.com>
2021-03-17 18:24:54 +0300

    Create nemo.js

---
+++ Objects/nemo.js
@@ -0,0 +1,21 @@
+// let fish = ['Dory', 'Marlin', 'Gill', 'Nemo', 'Bruce'];
+
+// for (let i = 0; i < fish.length; i++) {
+
+//     console.log(fish[i])
+//     if (fish[i] === "Nemo") {
+//         break;
+//     }
+
+// }
+
+
+// console.log("mountain".slice(0,1).toUpperCase() + "mountain".slice(1));
+// const publication = "launchschool"; publication[0].toUpperCase();
+// console.log(publication);
+// console.log(publication[0].toUpperCase());
+
+
+let array = ["mountain","mountain","mountain"];
+
+console.log(array.join(' and ', 'also'));
