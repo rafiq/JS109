68e73c83132940a4f271bd7acab08385949a838c
Rafiq Kamal <71498824+Rafiq@users.noreply.github.com>
2021-03-21 15:36:38 +0300

    Create carKeys.js

---
+++ Objects/carKeys.js
@@ -0,0 +1,12 @@
+let vehicle = {
+    manufacturer: 'Tesla',
+    model: 'Model X',
+    year: 2015,
+    range: 295,
+    seats: 7
+  };
+
+  for (let values of vehicle) {
+      console.log(values);
+  }
+//   console.log(Object.keys(vehicle));
