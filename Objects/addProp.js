569cebfa0e393b2a1c7d6ead07f2347bf167cb55
Rafiq Kamal <71498824+Rafiq@users.noreply.github.com>
2021-03-27 16:22:55 +0300

    Create addProp.js

---
+++ Objects/addProp.js
@@ -0,0 +1,13 @@
+let fido = {
+    name: 'Fido',
+    species: 'Labrador Retriever',
+    color: 'brown',
+    weight: 16,
+  };
+
+  // Add property 'age'.
+  // Add property 'favorite food'.
+
+  fido["age"] = 0;
+  fido["favorite food"] = "Steak";
+  console.log(fido);
