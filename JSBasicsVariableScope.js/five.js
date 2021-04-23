adab220559c3da880f61bb119c8229322cb91a1d
Rafiq Kamal <71498824+Rafiq@users.noreply.github.com>
2021-04-23 21:10:22 +0300

    Create five.js

---
+++ JSBasicsVariableScope.js/five.js
@@ -0,0 +1,12 @@
+// THe below code will return 1 and 2 to the terminal.
+function myFunction() {
+    let a = 1;
+
+    if (true) {
+      console.log(a);
+      let a = 2;
+      console.log(a);
+    }
+  }
+
+  myFunction();
